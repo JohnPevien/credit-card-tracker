f295ced96dabe794eefc880e6cfcb16b1c5dfa74
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-03-17 08:26:43 +0800

    Add sidebar and datatable

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Home, CreditCard, Users, ShoppingBag, FileText } from "lucide-react";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import { useState } from "react";
+
+const navLinks = [
+  {
+    label: "Home",
+    href: "/",
+    icon: <Home className="w-5 h-5 text-neutral-700 dark:text-neutral-200" />,
+  },
+  {
+    label: "Credit Cards",
+    href: "/credit-cards",
+    icon: (
+      <CreditCard className="w-5 h-5 text-neutral-700 dark:text-neutral-200" />
+    ),
+  },
+  {
+    label: "Persons",
+    href: "/persons",
+    icon: <Users className="w-5 h-5 text-neutral-700 dark:text-neutral-200" />,
+  },
+  {
+    label: "Purchases",
+    href: "/purchases",
+    icon: (
+      <ShoppingBag className="w-5 h-5 text-neutral-700 dark:text-neutral-200" />
+    ),
+  },
+  {
+    label: "Transactions",
+    href: "/transactions",
+    icon: (
+      <FileText className="w-5 h-5 text-neutral-700 dark:text-neutral-200" />
+    ),
+  },
+];
+
+export default function AppSidebar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Sidebar open={open} setOpen={setOpen} animate={true}>
+      <SidebarBody className="flex flex-col gap-4">
+        <div className="flex items-center gap-2 mb-6">
+          <CreditCard className="h-6 w-6 text-neutral-800 dark:text-neutral-200" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          {navLinks.map((link) => (
+            <SidebarLink key={link.href} link={link} />
+          ))}
+        </div>
+      </SidebarBody>
+    </Sidebar>
+  );
+}
