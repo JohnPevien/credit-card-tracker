333661363dd0cf281a9d6036e166f5cad56fe725
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-06-06 00:26:56 +0800

    Update modal backdrop opacity

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -37,7 +37,7 @@ export default function Modal({
       className="fixed inset-0 flex items-center justify-center z-50 p-4"
       onClick={handleBackdropClick}
     >
-      <div className="fixed inset-0 bg-black bg-opacity-50" />
+      <div className="fixed inset-0 bg-black opacity-80" />
       <div className={`relative  p-6 rounded-lg shadow-lg w-full ${MAX_WIDTH_CLASSES[maxWidth]} ${className}`}>
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-bold">{title}</h2>
