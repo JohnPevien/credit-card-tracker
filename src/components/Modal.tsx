a1b2a7ffa4f3edcb1d43e64c8345e5e2550143b1
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-06-09 22:59:15 +0800

    Add ARIA attributes to modal for accessibility

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -34,6 +34,8 @@ export default function Modal({
 
   return (
     <div 
+      role="dialog"
+      aria-modal="true"
       className="fixed inset-0 flex items-center justify-center z-50 p-4"
       onClick={handleBackdropClick}
     >
