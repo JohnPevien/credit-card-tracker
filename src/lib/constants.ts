82f983b611b29564a50c7ddaa77f0e1188c7a05e
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-06-17 23:41:14 +0800

    Refactors credit cards page
    - separate credit card form
    - move db request to service file
    - extract constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const PHILIPPINE_BANKS = [
+  "BDO",
+  "BPI",
+  "Metrobank",
+  "PNB",
+  "Security Bank",
+  "UnionBank",
+  "RCBC",
+  "Eastwest Bank",
+  "Citibank",
+  "HSBC",
+  "Maybank",
+  "China Bank",
+];
